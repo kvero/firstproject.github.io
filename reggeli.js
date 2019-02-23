5967dbdcce8af5eaaa4e183d6d07c61f3f0a6d17
kvero <kolossav@gmail.com>
2019-02-23 17:16:48 +0100

    merge

--- reggeli.js
+++ reggeli.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let szamlalo = 0
 
 $('.up').on('click', (event) =>  {
@@ -19,25 +18,3 @@ $('input[type="submit"]').on('click', (event) => {
   $('ul').append(`<li>${listname}</li>`);
 });
 
-=======
-let szamlalo = 0
-
-$('.up').on('click', (event) =>  {
-  szamlalo = szamlalo + 1
-$('h2').html(szamlalo)
-});
-
-
-$('.down').on('click', (event) => {
-  szamlalo = szamlalo - 1
-  $('h2').html(szamlalo)
-});
-
-
-$('input[type="submit"]').on('click', (event) => {
-  event.preventDefault();
-  let listname = $('input[type="text"]').val();
-  $('ul').append(`<li>${listname}</li>`);
-});
-
->>>>>>> 91173763fbf5c9988fc0277cec107653cbdc40d5
